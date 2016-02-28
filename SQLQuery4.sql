002a219a8c0dce48b66a90aa120f91184fcc661a
Zuzka <zuzka.papalova@gmail.com>
2016-02-28 17:06:03 +0100

    admin role

---
+++ SQLQuery4.sql
@@ -0,0 +1,1 @@
+﻿insert into Roles values(1,'admin')
---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Roles] (
+    [roles_id] INT           NOT NULL,
+    [name]     NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([roles_id] ASC)
+);
+
