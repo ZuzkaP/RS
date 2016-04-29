6703f933522aa0b0e3772836f5249ab14ba8df33
BeePM <tomas.blanarik@gmail.com>
2016-04-29 14:23:27 +0200

    missing class

---
+++ RS/Core/SQL.cs
@@ -0,0 +1,30 @@
+﻿using RS.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RS.Models
+{
+    public class SQL
+    {
+        private MainDB database;
+        private static SQL instance = new SQL();
+
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static SQL Instance { get { return instance; } }
+
+        /// <summary>
+        /// Get current session.
+        /// </summary>
+        public MainDB Database { get { return database; } }
+
+        private SQL()
+        {
+            database = new MainDB();
+        }
+    }
+}
