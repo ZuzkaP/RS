4edcffd2274ce12d808b810aac000ab7eecabd63
Zuzka <zuzka.papalova@gmail.com>
2016-05-01 19:49:31 +0200

    Trainings

---
+++ RS/Controllers/TrainingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RS.Core;
+using RS.Models;
+
+namespace RS.Controllers
+{
+    public class TrainingsController : Controller
+    {
+        private MainDB db = new MainDB();
+
+
+        // GET: Trainings/Create
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
